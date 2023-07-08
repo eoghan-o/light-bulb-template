c4b7485dc1b397f160cd4534a8d2be44060ed0c1
Hogchops <31632064+Hogchops@users.noreply.github.com>
2023-07-08 17:21:06 -0400

    Create containers.py

---
+++ containers.py
@@ -0,0 +1,25 @@
+"""
+Created on Sat Jul  8 17:17:42 2023
+
+@author: 
+@version:
+"""
+
+def main():
+    # interesting code
+    
+
+
+def input_function():
+    # more interesting code
+
+
+main()
+
+
+'''
+Remember, when you are done writing a function, type:
+    """
+below the function header and you will be prompted with
+generate docstring, which will help you comment.
+'''
